06f8601958a7813eb69bee8f457c8d45d36a699e
Jamie Henson <jhenson47@gmail.com>
2017-07-15 16:41:57 +0100

    GA ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -37,7 +37,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-31560980-1`
       }
     },
     `gatsby-plugin-offline`,
